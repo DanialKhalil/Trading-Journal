fa4d9f3abf14b1b07482b72163b1cf76d607f2b9
imanafiq12 <s211502836@studentmail.unimap.edu.my>
2022-07-19 13:22:47 +0800

    aku dah letak time

--- # mini project.py
+++ # mini project.py
@@ -6,6 +6,21 @@ from cgitb import text
 from tkinter import *
 from tkinter.tix import TEXT
 from turtle import bgcolor, right
+import time
+
+# start clock config________________________________________
+
+text_font= ("Boulder", 18, 'bold')
+background = "pink"
+foreground= "#363529"
+border_width = 1
+
+def digital_clock(): 
+   time_live = time.strftime("%H:%M:%S")
+   clock.config(text=time_live) 
+   clock.after(2000, digital_clock)
+
+# END clock config________________________________________
 
 window = Tk()
 
@@ -15,6 +30,7 @@ height= window.winfo_screenheight()
 #window.geometry("%dx%d" % (width, height))# fullscreen
 
 
+
 # START FRAME ADDJOURNAL ______________________________________________________________________________
 
 ADDJOURNAL = LabelFrame(window, highlightbackground="blue", highlightthickness=2,text='ADD JOURNAL')
@@ -22,46 +38,51 @@ ADDJOURNAL.grid(row=0,column=0)    # Frame for add journal
 
 
 
-labelINSTRUMENT=Label(ADDJOURNAL,text='INSTRUMENT :',width=50,anchor='w',padx=50,pady=5)
+labelINSTRUMENT=Label(ADDJOURNAL,text='INSTRUMENT :',width=10,anchor='w',padx=10,pady=5)
 labelINSTRUMENT.grid(row=0,column=0)
-de_instrument=Entry(ADDJOURNAL,bd=2,width=30)
-de_instrument.grid(row=0,column=0)
+de_instrument=Entry(ADDJOURNAL,bd=2,width=50)
+de_instrument.grid(row=0,column=1,columnspan=3)
 
-labelmarket_position=Label(ADDJOURNAL,text='POSITION :',width=50,anchor='w',padx=50,pady=5)
+labelmarket_position=Label(ADDJOURNAL,text='POSITION :',width=10,anchor='w',padx=10,pady=5)
 labelmarket_position.grid(row=1,column=0)
-de_market_position=Entry(ADDJOURNAL,bd=2,width=30)
-de_market_position.grid(row=1,column=0)
+de_market_position=Entry(ADDJOURNAL,bd=2,width=50)
+de_market_position.grid(row=1,column=1)
 
-labelLOT_SIZE=Label(ADDJOURNAL,text='LOT_SIZE :',width=50,anchor='w',padx=50,pady=5)
+labelLOT_SIZE=Label(ADDJOURNAL,text='LOT_SIZE :',width=10,anchor='w',padx=10,pady=5)
 labelLOT_SIZE.grid(row=2,column=0)
-de_LOT_SIZE=Entry(ADDJOURNAL,bd=2,width=30)
-de_LOT_SIZE.grid(row=2,column=0)
+de_LOT_SIZE=Entry(ADDJOURNAL,bd=2,width=50)
+de_LOT_SIZE.grid(row=2,column=1)
 
-labelRISK=Label(ADDJOURNAL,text='RISK :',width=50,anchor='w',padx=50,pady=5)
+labelRISK=Label(ADDJOURNAL,text='RISK :',width=10,anchor='w',padx=10,pady=5)
 labelRISK.grid(row=3,column=0)
-de_RISK=Entry(ADDJOURNAL,bd=2,width=30)
-de_RISK.grid(row=3,column=0)
+de_RISK=Entry(ADDJOURNAL,bd=2,width=50)
+de_RISK.grid(row=3,column=1)
 
-labelREWARD=Label(ADDJOURNAL,text='REWARD :',width=50,anchor='w',padx=50,pady=5)
+labelREWARD=Label(ADDJOURNAL,text='REWARD :',width=10,anchor='w',padx=10,pady=5)
 labelREWARD.grid(row=4,column=0)
-de_REWARD=Entry(ADDJOURNAL,bd=2,width=30)
-de_REWARD.grid(row=4,column=0)
+de_REWARD=Entry(ADDJOURNAL,bd=2,width=50)
+de_REWARD.grid(row=4,column=1)
 
-labelPROFIT=Label(ADDJOURNAL,text='PROFIT :',width=50,anchor='w',padx=50,pady=5)
+labelPROFIT=Label(ADDJOURNAL,text='PROFIT :',width=10,anchor='w',padx=10,pady=5)
 labelPROFIT.grid(row=5,column=0)
-de_PROFIT=Entry(ADDJOURNAL,bd=2,width=30)
-de_PROFIT.grid(row=5,column=0)
+de_PROFIT=Entry(ADDJOURNAL,bd=2,width=50)
+de_PROFIT.grid(row=5,column=1)
 
-labelLOSS=Label(ADDJOURNAL,text='LOSS :',width=50,anchor='w',padx=50,pady=5)
+labelLOSS=Label(ADDJOURNAL,text='LOSS :',width=10,anchor='w',padx=10,pady=5)
 labelLOSS.grid(row=6,column=0)
-de_LOSS=Entry(ADDJOURNAL,bd=2,width=30)
-de_LOSS.grid(row=6,column=0)
+de_LOSS=Entry(ADDJOURNAL,bd=2,width=50)
+de_LOSS.grid(row=6,column=1)
 
-labelSETUP=Label(ADDJOURNAL,text='SETUP :',width=50,anchor='w',padx=50,pady=5)
+labelSETUP=Label(ADDJOURNAL,text='SETUP :',width=10,anchor='w',padx=10,pady=5)
 labelSETUP.grid(row=7,column=0)
-de_SETUP=Entry(ADDJOURNAL,bd=2,width=30)
-de_SETUP.grid(row=7,column=0)
+de_SETUP=Entry(ADDJOURNAL,bd=2,width=50)
+de_SETUP.grid(row=7,column=1)
 
+submitbutton= Button(ADDJOURNAL,text='Submit',bg='pink') #tak buat command lagi
+submitbutton.grid(row=8,column=1,sticky=N)
+
+clearbutton= Button(ADDJOURNAL,text='Clear',bg='pink') #tak buat command lagi
+clearbutton.grid(row=8,column=1,sticky=W)
 
 # END FRAME ADDJOURNAL__________________________________________________________________________
 
@@ -69,6 +90,10 @@ de_SETUP.grid(row=7,column=0)
 
 FRAMEPERFOMANCE = LabelFrame(window, highlightbackground="gray23", highlightthickness=1,text='DASH BOARDPERFOMANCE',bg='wheat1')
 FRAMEPERFOMANCE.grid(row=0,column=1,padx=10)    # Frame for PERFOMANCE BOARDDASH
+
+    #clock
+clock = Label(FRAMEPERFOMANCE, font=text_font, bg=background, fg=foreground, bd=border_width) 
+clock.pack()
 
      #total profit_______________________________________
 labelPROFIT=Label(FRAMEPERFOMANCE,text='TOTAL PROFIT',width=50,font=25,bg='wheat1')
@@ -109,28 +134,21 @@ label_comment=Label(FRAMEPERFOMANCE,text=comment)
 label_comment.pack(side=LEFT)
 
 
+
 # END FRAME PERFOMANCE__________________________________________________________________________
 
 # START FRAME DASHBOARDPERFOMANCE________________________________________________________________________
 
-JOURNAL = LabelFrame(window, highlightbackground="gray23", highlightthickness=1,text='JOURNAL HISTORY',bg='wheat1')
+JOURNAL = LabelFrame(window, highlightbackground="gray23", highlightthickness=1,text='JOURNAL HISTORY IMAN POWER',bg='wheat1')
 JOURNAL.grid(row=2,column=0,columnspan=3,padx=10)    # Frame for PERFOMANCE BOARDDASH
 
 textboxarea=Text(JOURNAL,height=25,width=100,bg='wheat1')
 textboxarea.pack()
 
 # END FRAME PERFOMANCE__________________________________________________________________________
+    
+    
 
-
+digital_clock()
 window.mainloop() #mainloop // kalau mainloop tak ada coding tak jalan
-#last try
-#pundek punya github
 
-#try
-#tambah ayat
-#akjsnxkanskjxdnkasjnx
-#iman hensem
-
-#danialhensem
-#zairie
-#0283838383838
